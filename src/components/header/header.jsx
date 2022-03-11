482844424124ed40736c60047bfcf5e140f3de23
yaroslav grigorev <yaroslav.purple@gmail.com>
2022-03-11 22:33:38 +0300

    nav menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { navItems } from '../../const/const';
 import NavItem from './nav-item';
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+  const [activePage, _] = useState(window.location.pathname);
+
   return (
     <React.Fragment>
       <header className="header">
         <div className="header__content">
-          <div className="header__menu menu">
-            <div className="menu__icon icon-menu"><span></span> <span></span> <span></span></div>
-            <nav className="menu__body _active">
+          <div 
+            className="header__menu menu"
+            onClick={() => setIsActive(!isActive)}>
+            <div className={isActive ? `menu__icon icon-menu _active` : `menu__icon icon-menu`}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <nav className={isActive ? `menu__body _active` : `menu__body`}>
               <ul className="menu__list">
-                {navItems.map(item => <NavItem navItem={item} key={item.title}/>)}
+                {navItems.map(item => <NavItem navItem={item} key={item.title} activePage={activePage}/>)}
               </ul>
             </nav>
           </div>
--- src/components/header/nav-item.jsx
+++ src/components/header/nav-item.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavItem = ({navItem}) => {
+const NavItem = ({navItem, activePage}) => {
   return (
     <React.Fragment>
       <li>
-        <Link to={navItem.url} className="menu__link">{navItem.title}</Link>
+        <Link to={navItem.url} className={activePage === navItem.url ? `menu__link _active` : `menu__link`}>{navItem.title}</Link>
       </li>
     </React.Fragment>
   );
